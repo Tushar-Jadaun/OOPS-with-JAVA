154db0ca2b714b92140fce7b3a79e38fb564c117
tushar jadaun <152781603+Tushar-Jadaun@users.noreply.github.com>
2024-08-11 23:12:13 +0530

    use of string

---
+++ third.java
@@ -0,0 +1,18 @@
+public class third {
+    public static void main(String[] args) 
+    {
+        String a =new String("value tushar  1 ");
+        String b ="value 2";
+        b="value 3";
+        
+        System.out.println(b.contains("al"));
+        
+        System.out.println("hello");
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+        System.out.println(a.endsWith("1"));
+        System.out.println(a.indexOf("ue"));
+        System.out.println(a.replace("tushar","Jadaun"));
+    }
+    
+}
